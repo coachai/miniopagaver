fa2373d4dbe76ca372cf69de579e6a7a2109ff43
HUa <huachai2011@hotmail.com>
2016-11-09 10:31:45 +0100

    Pets

---
+++ Pets/Pets/Pet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pets
+{
+    class Pet
+    {
+        public string name;
+        public string species;
+
+        public Pet(string name, string species)
+            { this.name = name;
+              this.species = species;
+            }
+    }
+}
---
+++ Pets/Pets/Specie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pets
+{
+    public class Specie
+	{
+        public string name;
+        public int numberOfEyes;
+        public int numberOfLegs;
+     
+     public Specie(string name, int numberOfEyes, int numberOfLegs)
+    {   this.name = name;
+        this.numberOfLegs = numberOfLegs;
+        this.numberOfEyes = numberOfEyes;
+    }
+	}
+}
