423bc392410b44c750886f8517cc263d136e6eb5
HUa <huachai2011@hotmail.com>
2016-11-22 14:18:58 +0100

    Calculator

---
+++ App3/App3/App3/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void ButtonPress()
+        {// OnBackButtonPressed += ButtonPress.ClassId="Button1" ; 
+        }
+    }
+}
