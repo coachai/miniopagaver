0da34d1aae0851bd162178efec0d7f6ea1aec098
HUa <huachai2011@hotmail.com>
2016-11-23 14:52:51 +0100

    XAML

---
+++ XamlSamples/XamlSamples/XamlSamples/HelloXamlPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XamlSamples
+{
+    public partial class HelloXamlPage : ContentPage
+    {
+        public HelloXamlPage()
+        {
+            InitializeComponent();
+        }
+
+        public void OnSlideValueChanged(object sender,
+                          ValueChangedEventArgs args)
+        { valueLabel.Text = args.NewValue.ToString("F3"); }
+
+        async void OnButtonClicked(object sende, EventArgs args)
+        { Button button = (Button)sende;
+            await DisplayAlert("Clicked!",
+        "The button labeled '" + button.Text + "' has been clicked",
+        "OK");
+        }
+    }
+    }
+
