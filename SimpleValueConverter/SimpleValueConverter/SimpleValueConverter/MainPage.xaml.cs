e534106d7684f4a08300466b0deb174789d9a204
HUa <huachai2011@hotmail.com>
2016-12-02 14:47:35 +0100

    ValueConverter

---
+++ SimpleValueConverter/SimpleValueConverter/SimpleValueConverter/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using SimpleValueConverter.Converters;
+
+namespace SimpleValueConverter
+{  
+    public partial class MainPage : ContentPage
+    {
+        private SimpleViewModel svm;
+        private intScaleValueConverter intScaleConverter = new intScaleValueConverter();
+
+        public MainPage(SimpleViewModel svm)
+        {
+            InitializeComponent();
+            this.svm = svm;
+            BindingContext = this.svm;
+            //slider3.SetBinding<SimpleViewModel>(Slider.ValueProperty, vm => vm.MyValue, BindingMode.TwoWay, intScaleConverter);
+            //slider4.SetBinding<SimpleViewModel>(Slider.ValueProperty, vm=>vm.MyValue,BindingMode.OneWay);
+            oneLabel.SetBinding<SimpleViewModel>(Label.TextProperty, vm => vm.MyValue, BindingMode.OneWay, intScaleConverter);
+        }
+    }
+}
---
+++ SimpleValueConverter/SimpleValueConverter/SimpleValueConverter/SimpleViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SimpleValueConverter
+{
+   public class SimpleViewModel : INotifyPropertyChanged
+    {
+        private double myValue = 1;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public double MyValue
+        {
+            get
+            {
+                return myValue;
+            }
+
+            set
+            {   if (myValue != value)
+                { myValue = value;
+                    onPropertyChanged();
+                }
+              
+            }
+        }
+
+        public void onPropertyChanged([CallerMemberName] string propertyName= "")
+        { var ev = PropertyChanged;
+            if (ev != null)
+            { ev(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+
+    }
+}
